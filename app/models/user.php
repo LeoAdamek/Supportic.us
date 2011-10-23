a35d9d0e437124cfb673119b424548f000ca527f
Leo Adamek <leo@adamek.me>
2011-10-23 14:03:02 +0100

    Added first version of name validation
    Cleanup

--- app/models/user.php
+++ app/models/user.php
@@ -12,6 +12,10 @@ class User extends AppModel {
 				//'last' => false, // Stop validation after this rule
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
+			'illegalCharacters' => array(
+				'rule' => '/^\p{L}+$/u', // Validate string for all letters from all character sets in all languages of the world.
+				'message' => 'Your name contains disallowed characters',
+			),
 		),
 		'addressName' => array(
 			'notempty' => array(
