fecaad15408e526540245219b70938a22d463b79
Leo Adamek <leo@adamek.me>
2011-11-06 11:52:38 +0000

    Fixes to the lexical analysis of KB system

--- app/controllers/knowledge_bases_controller.php
+++ app/controllers/knowledge_bases_controller.php
@@ -2,6 +2,7 @@
 class KnowledgeBasesController extends AppController {
 
 	var $name = 'KnowledgeBases';
+	var $uses = array('KnowledgeBase'); // Because CakePHP has trouble with this word.
 	var $helpers  = array('Time');
 
 	function beforeFilter(){
@@ -15,7 +16,7 @@ class KnowledgeBasesController extends AppController {
 			$this->KnowledgeBase->Organisation->id = $oid;
 			if($this->KnowledgeBase->Organisation->exists()){
 				
-				$articles = $this->Kn
+				// Get the Articles
 
 			}else{
 				$this->Session->setFlash("This Organisation does not exist.");
---
+++ app/models/knowledge_base.php
@@ -0,0 +1,42 @@
+<?php
+class KnowledgeBase extends AppModel {
+	var $name = 'KnowledgeBase';
+	var $displayField = 'title';
+	var $validate = array(
+		'title' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+	var $belongsTo = array(
+		'Organisation' => array(
+			'className' => 'Organisation',
+			'foreignKey' => 'organisation_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'User' => array(
+			'className' => 'User',
+			'foreignKey' => 'user_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Category' => array(
+			'className' => 'Category',
+			'foreignKey' => 'category_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+}
