8aa5781eb70d5b90f4550341c507a57ecd3cc039
Leo Adamek <leo@adamek.me>
2011-11-06 20:57:05 +0000

    Rich text editing added to Article Creation

--- app/views/knowledge_bases/add.ctp
+++ app/views/knowledge_bases/add.ctp
@@ -1,10 +1,17 @@
 <h2>Create a new Article</h2>
 
 	<?php
+		echo $this->Html->script('tiny_mce/tiny_mce');
+		echo $this->Html->script('tiny_mce/jquery.tinymce');
+		echo $this->Html->script('kb/tinymce');
+	?>
+
+
+	<?php
 		echo $this->Form->create('KnowledgeBase', array('controller' => 'KnowledgeBase', 'action' => "add/{$org_id}"));
 		echo $this->Form->input('title');
 		echo $this->Form->input('body', array(
-			'class' => 'tinyMCE',
+			'class' => 'tinymce',
 			'cols' => '20'
 		));
 		echo $this->Form->input('category_id', array(
---
+++ app/webroot/js/kb/tinymce.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+	$('.tinymce').tinymce({
+		script: 'https://supportic.us/js/tiny_mce/tiny_mce.js',
+		theme: "advanced",
+		skin:	"o2k7",
+		plugins: "iespell,inlinepopus,insertdatetime,print,paste,directionality,fullscreen,visualchars",
+
+		theme_advanced_buttons1: "bold,italic,strikethrough,|,fontsizeselect,fontselect",
+		theme_advanced_buttons2: "cut,copy,paste,|,search,replace,|,bullist,|,numlist,|,outdent,indent,blockquote,|,undo,redo",
+		theme_advanced_buttons3: "link,unlink,anchor,image,cleanup,help,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+		
+		theme_advanced_toolbar_location: "top",
+		theme_advanced_toolbar_align: "left",
+		theme_advanced_statusbar_location: "bottom",
+		theme_advanced_resizing: true
+	});
+});
